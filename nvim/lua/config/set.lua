c27427be1902b0ed2e4b75cfe46e2c4c0cb1de68
coder114514 <98397499+coder114514@users.noreply.github.com>
2024-08-13 01:36:23 +0800

    settings update and key mappings update

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -8,5 +8,20 @@ vim.opt.expandtab=true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.incsearch = true
+vim.opt.hlsearch = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = vim.fn.stdpath('data') .. "/undodir"
+vim.opt.undofile = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
 
 vim.g.mapleader = " "
