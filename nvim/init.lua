8ba48fbc117ec56d046af93a47566ec29d3ecd1c
coder114514 <98397499+coder114514@users.noreply.github.com>
2024-08-18 00:39:13 +0800

    make nvim config more vanilla

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
--- Bootstrap lazy.nvim
+---- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -13,18 +13,61 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
         os.exit(1)
     end
 end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.runtimepath:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
+---- Make sure to setup `mapleader` and `maplocalleader` before
+---- loading lazy.nvim so that mappings are correct.
+---- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-require("config.opt")
-require("config.remap")
+-- gv: reuse last selection
+vim.keymap.set("v", "J", ":move '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":move '<-2<CR>gv=gv")
 
--- Setup lazy.nvim
+vim.keymap.set("n", "J", "mzJ`z") -- mz,J,`z
+
+vim.keymap.set("v", "<leader>d", '"_d')
+vim.keymap.set("n", "<leader>d", '"_d')
+
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y')
+vim.keymap.set("n", "<leader>p", '"+p')
+vim.keymap.set("n", "<leader>P", '"+P')
+
+vim.keymap.set("c", "term", "split | term")
+
+-- options
+
+vim.cmd.colorscheme("desert")
+
+vim.opt.guicursor = "a:block-blinkon0"
+
+vim.opt.nu = true
+vim.opt.hidden = false
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+
+vim.opt.cursorline = true
+vim.opt.listchars = { space = '·', nbsp = '⎵', tab = '▸-' }
+
+vim.opt.swapfile = false
+vim.opt.undodir = vim.fn.stdpath('data') .. "/undodir"
+vim.opt.undofile = true
+
+vim.opt.isfname:append("@-@")
+
+-- https://vim-jp.org/vimdoc-en/indent.html#cinoptions-values
+vim.opt.cinoptions = "l1N-sE-st0Lsg0"
+
+---- Setup lazy.nvim
 require("lazy").setup({
     spec = {
         -- import your plugins
@@ -32,7 +75,7 @@ require("lazy").setup({
     },
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
-    install = { colorscheme = { "habamax" } },
+    install = { colorscheme = { "desert" } },
     -- automatically check for plugin updates
     checker = { enabled = true },
 })
