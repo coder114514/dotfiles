8b24c4c2c1794f36e5179ea26382f2dc52185541
coder114514 <98397499+coder114514@users.noreply.github.com>
2024-08-18 12:29:09 +0800

    commit

--- nvim/init.lua
+++ nvim/init.lua
@@ -22,21 +22,17 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 -- gv: reuse last selection
-vim.keymap.set("v", "J", ":move '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":move '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", [[:move '>+1<CR>gv=gv]])
+vim.keymap.set("v", "K", [[:move '<-2<CR>gv=gv]])
 
 vim.keymap.set("n", "J", "mzJ`z") -- mz,J,`z
 
-vim.keymap.set("v", "<leader>d", '"_d')
-vim.keymap.set("n", "<leader>d", '"_d')
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
-vim.keymap.set("n", "<leader>y", '"+y')
-vim.keymap.set("v", "<leader>y", '"+y')
-vim.keymap.set("n", "<leader>Y", '"+Y')
-vim.keymap.set("n", "<leader>p", '"+p')
-vim.keymap.set("n", "<leader>P", '"+P')
-
-vim.keymap.set("c", "term", "split | term")
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set("n", "<leader>p", [["+p]])
+vim.keymap.set("n", "<leader>P", [["+P]])
 
 -- options
 
