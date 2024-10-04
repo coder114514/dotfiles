3bbfea232a4ee83064c9029a6adeacc109cce1a5
coder114514 <98397499+coder114514@users.noreply.github.com>
2024-10-04 14:50:34 +0800

    update emacs config

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -1,40 +1,20 @@
-(package-initialize)
-
-(setq inhibit-startup-screen t)
-(setq visible-bell 1)
-
-; (add-hook 'window-setup-hook 'toggle-frame-maximized t)
 (add-to-list 'default-frame-alist '(font . "Sarasa Mono SC"))
 
-(global-display-line-numbers-mode)
-(global-hl-line-mode)
+;;; package
 
-(tool-bar-mode 0)
-(menu-bar-mode 0)
-(scroll-bar-mode 0)
-(column-number-mode 1)
-(show-paren-mode 1)
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
 
-(add-to-list 'load-path (expand-file-name "lisp/" (file-name-directory load-file-name)))
-
-;;; Elpa Packs
-(require 'rc-pack)
+(require 'use-package)
+(setq use-package-always-ensure t)
 
 ;;; ido
-(rc/require 'smex 'ido-completing-read+)
-
-(require 'ido-completing-read+)
-
-(ido-mode 1)
-(ido-everywhere 1)
+(use-package ido-completing-read+)
 (ido-ubiquitous-mode 1)
 
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
-
-
 ;;; Paredit
-(rc/require 'paredit)
+(use-package paredit)
 
 (defun rc/turn-on-paredit ()
   (interactive)
@@ -47,16 +27,13 @@
 (add-hook 'scheme-mode-hook      'rc/turn-on-paredit)
 (add-hook 'racket-mode-hook      'rc/turn-on-paredit)
 
-
-;;; Emacs lisp
+;;; emacs lisp
 (add-hook 'emacs-lisp-mode-hook
-          '(lambda ()
-             (local-set-key (kbd "C-c C-j")
-                            (quote eval-print-last-sexp))))
-(add-to-list 'auto-mode-alist '("Cask" . emacs-lisp-mode))
+          (lambda ()
+            (local-set-key (kbd "C-c C-j")
+                           (quote eval-print-last-sexp))))
 
-
-;;; c-mode
+;;; C
 (setq-default c-basic-offset 4
               c-default-style '((java-mode . "java")
                                 (awk-mode . "awk")
@@ -66,32 +43,35 @@
                          (interactive)
                          (c-toggle-comment-style -1))) ; line comment style
 
-
 ;;; multiple cursors
-(rc/require 'multiple-cursors)
+(use-package multiple-cursors
+  :bind
+  ("C-S-c C-S-c" . mc/edit-lines)
+  ("C->"         . mc/mark-next-like-this)
+  ("C-<"         . mc/mark-previous-like-this)
+  ("C-c C-<"     . mc/mark-all-like-this)
+  ("C-\""        . mc/skip-to-next-like-this)
+  ("C-:"         . mc/skip-to-previous-like-this))
 
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C->")         'mc/mark-next-like-this)
-(global-set-key (kbd "C-<")         'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<")     'mc/mark-all-like-this)
-(global-set-key (kbd "C-\"")        'mc/skip-to-next-like-this)
-(global-set-key (kbd "C-:")         'mc/skip-to-previous-like-this)
-
-
-
-;;; Custom
+;;; custom
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(cursor-type 'bar)
- '(display-line-numbers-type t)
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(display-line-numbers-type 'relative)
  '(global-display-line-numbers-mode t)
+ '(global-hl-line-mode t)
+ '(ido-everywhere t)
+ '(ido-mode 'both nil (ido))
+ '(inhibit-startup-screen t)
  '(make-backup-files nil)
- '(package-selected-packages '(ido-completing-read+ smex))
- '(whitespace-style
-   '(face tabs spaces trailing space-before-tab newline indentation empty space-after-tab space-mark tab-mark)))
+ '(package-selected-packages '(multiple-cursors paredit ido-completing-read+))
+ '(scroll-conservatively 50)
+ '(tool-bar-mode nil)
+ '(visible-bell t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
