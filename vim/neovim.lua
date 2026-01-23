7d1c0f1ec6997ad477753d0b911e592dedf2299c
coder114514 <98397499+coder114514@users.noreply.github.com>
2026-01-24 04:43:40 +0800

    big vim config update, a power-vanilla style config, hopefully once and for all

--- vim/neovim.lua
+++ vim/neovim.lua
@@ -2,11 +2,17 @@
 ---- Configurable Variables before loading this script:
 ---    vim.g.have_nerd_font
 ----   vim.g.lazyroot
+---    vim.g.lsps
 -----------------------------------------------------------------------------
 
 local scriptpath = debug.getinfo(1, 'S').source:sub(2)
 local scripthome = vim.fn.fnamemodify(scriptpath, ':h')
 local is_windows = package.config:sub(1, 1) == '\\'
+if is_windows then
+    scripthome = scripthome:gsub('/', '\\')
+else
+    scripthome = scripthome:gsub('\\', '/')
+end
 local path_sep = is_windows and '\\' or '/'
 
 package.path = package.path .. ';' .. scripthome .. path_sep .. '?.lua'
@@ -14,23 +20,50 @@ package.path = package.path .. ';' .. scripthome .. path_sep .. '?.lua'
 vim.cmd.source(scripthome .. path_sep .. 'vim.vim')
 
 vim.opt.undofile = true -- for undo tree
+
 vim.api.nvim_create_user_command('Diags', function()
     vim.diagnostic.setqflist()
 end, { desc = 'Add all diagnostics to the quickfix list.' })
 
-------------------------------------------------------------------------------
----- Bootstrap lazy.nvim
-------------------------------------------------------------------------------
-local lazyroot = vim.g.lazyroot or vim.fn.stdpath "data" .. path_sep .. "lazy"
-local lazypath = lazyroot .. path_sep .. "lazy.nvim"
+vim.cmd.packloadall()
+
+vim.keymap.set('n', '<leader>th', function()
+    print('Inlay Hints Not Loaded')
+end, { desc = 'LSP: [T]oggle Inlay [H]ints' })
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    desc = 'Lsp Actions',
+    group = vim.api.nvim_create_augroup('lsp_stuff', { clear = true }),
+    callback = function(event)
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+        if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+            vim.keymap.set('n', '<leader>th', function()
+                vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+            end, { desc = '[T]oggle Inlay [H]ints', buffer = event.buf })
+        end
+    end,
+})
+
+if vim.g.lsps then
+    for _, lsp in pairs(vim.g.lsps) do
+        local name, config = lsp[1], lsp[2]
+        vim.lsp.enable(name)
+        if config then
+            vim.lsp.config(name, config)
+        end
+    end
+end
+
+local lazyroot = vim.g.lazyroot or vim.fn.stdpath 'data' .. path_sep .. 'lazy'
+local lazypath = lazyroot .. path_sep .. 'lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
     if vim.v.shell_error ~= 0 then
         vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out, "WarningMsg" },
-            { "\nPress any key to exit..." },
+            { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+            { out, 'WarningMsg' },
+            { '\nPress any key to exit...' },
         }, true, {})
         vim.fn.getchar()
         os.exit(1)
@@ -38,9 +71,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.runtimepath:prepend(lazypath)
 
-------------------------------------------------------------------------------
----- lazy options
-------------------------------------------------------------------------------
 local opts = {
     root = lazyroot,
     performance = {
@@ -50,17 +80,188 @@ local opts = {
         },
     },
     ui = {
-        icons = vim.g.have_nerd_font and {} or {
-            cmd = '', config = '', event = '', ft = '', init = '', keys = '', plugin = '',
-            runtime = '', require = '', source = '', start = '', task = '', lazy = ''
-        },
+        icons = vim.g.have_nerd_font and {} or { cmd = '', config = '', event = '', ft = '', init = '', keys = '', plugin = '', runtime = '', require = '', source = '', start = '', task = '', lazy = '' },
     },
     rocks = {
-        enabled = false
+        enabled = false,
     },
 }
 
-------------------------------------------------------------------------------
----- Setup lazy.nvim
-------------------------------------------------------------------------------
-require("lazy").setup("plugins", opts)
+local specs = {
+    {
+        'kdheepak/lazygit.nvim',
+        cmd = {
+            'LazyGit',
+            'LazyGitConfig',
+            'LazyGitCurrentFile',
+            'LazyGitFilter',
+            'LazyGitFilterCurrentFile',
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+        },
+        keys = {
+            { '<leader>lg', '<cmd>LazyGit<cr>', desc = '[L]azy[G]it' }
+        },
+    },
+
+    {
+        'nvim-treesitter/nvim-treesitter',
+        lazy = false,
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter-textobjects',
+        },
+        build = function()
+            require('nvim-treesitter.install').update({ with_sync = true })()
+        end,
+        config = function()
+            require('nvim-treesitter.install').prefer_git = false
+            -- @diagnostic disable-next-line: missing-fields
+            require('nvim-treesitter.configs').setup {
+                ensure_installed = { 'c', 'cpp', 'lua', 'luadoc', 'vim', 'vimdoc', 'query', 'markdown', 'markdown_inline' },
+                sync_install = false,
+                auto_install = false,
+                highlight = {
+                    enable = true,
+                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                    -- Instead of true it can also be a list of languages
+                    -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+                    --  If you are experiencing weird indenting issues, add the language to
+                    --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+                    additional_vim_regex_highlighting = { 'ruby' },
+                },
+                indent = { enable = true, disable = { 'ruby' } },
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+                        keymaps = {
+                            -- You can use the capture groups defined in textobjects.scm
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                            ["ic"] = "@class.inner",
+                            ["aa"] = "@parameter.outer", -- Select 'Around Argument'
+                            ["ia"] = "@parameter.inner", -- Select 'Inner Argument'
+                        },
+                    },
+                    swap = {
+                        enable = true,
+                        swap_next = {
+                            ["<leader>a"] = "@parameter.inner", -- Swap parameter right
+                        },
+                        swap_previous = {
+                            ["<leader>A"] = "@parameter.inner", -- Swap parameter left
+                        },
+                    },
+                    move = {
+                        enable = true,
+                        set_jumps = true, -- whether to set jumps in the jumplist
+                        goto_next_start = {
+                            ["]m"] = "@function.outer",
+                            ["]]"] = "@class.outer",
+                        },
+                        goto_next_end = {
+                            ["]M"] = "@function.outer",
+                            ["]["] = "@class.outer",
+                        },
+                        goto_previous_start = {
+                            ["[m"] = "@function.outer",
+                            ["[["] = "@class.outer",
+                        },
+                        goto_previous_end = {
+                            ["[M"] = "@function.outer",
+                            ["[]"] = "@class.outer",
+                        },
+                    },
+                },
+            }
+        end,
+    },
+
+    {
+        'nvim-telescope/telescope.nvim',
+        lazy = false,
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+        },
+        config = function()
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>sh',       builtin.help_tags,   { desc = '[S]earch [H]elp' })
+            vim.keymap.set('n', '<leader>sk',       builtin.keymaps,     { desc = '[S]earch [K]eymaps' })
+            vim.keymap.set('n', '<leader>sf',       builtin.find_files,  { desc = '[S]earch [F]iles' })
+            vim.keymap.set('n', '<leader>ss',       builtin.builtin,     { desc = '[S]earch [S]elect Telescope' })
+            vim.keymap.set('n', '<leader>sw',       builtin.grep_string, { desc = '[S]earch current [W]ord' })
+            vim.keymap.set('n', '<leader>sg',       builtin.live_grep,   { desc = '[S]earch by [G]rep' })
+            vim.keymap.set('n', '<leader>sd',       builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+            vim.keymap.set('n', '<leader>sr',       builtin.resume,      { desc = '[S]earch [R]esume' })
+            vim.keymap.set('n', '<leader>s.',       builtin.oldfiles,    { desc = '[S]earch Recent Files ("." for repeat)' })
+            vim.keymap.set('n', '<leader><leader>', builtin.buffers,     { desc = '[ ] Find existing buffers' })
+
+            -- Slightly advanced example of overriding default behavior and theme
+            vim.keymap.set('n', '<leader>/', function()
+                -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+                builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+                    winblend = 10,
+                    previewer = false,
+                })
+            end, { desc = '[/] Fuzzily search in current buffer' })
+
+            -- It's also possible to pass additional configuration options.
+            --  See `:help telescope.builtin.live_grep()` for information about particular keys
+            vim.keymap.set('n', '<leader>s/', function()
+                builtin.live_grep {
+                    grep_open_files = true,
+                    prompt_title = 'Live Grep in Open Files',
+                }
+            end, { desc = '[S]earch [/] in Open Files' })
+
+            vim.keymap.set('n', '<leader>sn', function()
+                builtin.find_files { cwd = vim.fn.stdpath 'config'  }
+            end, { desc = '[S]earch [N]eovim files' })
+
+            vim.keymap.set('n', '<leader>sc', function()
+                builtin.find_files { cwd = scripthome, file_ignore_patterns = { '^pack/', '^pack\\'  } }
+            end, { desc = '[S]earch [C]onfig files' })
+
+            -- Replace some default LSP shortcuts with those of telescope and add better descriptions
+            vim.keymap.set('n',          'grn', vim.lsp.buf.rename,                    { desc = 'LSP: Re[n]ame' })
+            vim.keymap.set({ 'n', 'x' }, 'gra', vim.lsp.buf.code_action,               { desc = 'LSP: Code [A]ction' })
+            vim.keymap.set('n',          'grr', builtin.lsp_references,                { desc = 'LSP: Goto [R]eferences' })
+            vim.keymap.set('n',          'gri', builtin.lsp_implementations,           { desc = 'LSP: Goto [I]mplementation' })
+            vim.keymap.set('n',          'grd', builtin.lsp_definitions,               { desc = 'LSP: Goto [D]efinition' })
+            vim.keymap.set('n',          'grD', vim.lsp.buf.declaration,               { desc = 'LSP: Goto [D]eclaration' })
+            vim.keymap.set('n',          'grt', builtin.lsp_type_definitions,          { desc = 'LSP: [T]ype Definition' })
+            vim.keymap.set('n',          'gO',  builtin.lsp_document_symbols,          { desc = 'LSP: [O]pen Document Symbols' })
+            vim.keymap.set('n',          'gW',  builtin.lsp_dynamic_workspace_symbols, { desc = 'LSP: Open [W]orkspace Symbols' })
+        end,
+    },
+
+    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+    -- used for completion, annotations and signatures of Neovim APIs
+    {
+        'folke/lazydev.nvim',
+        ft = 'lua',
+        dependencies = {
+            { 'Bilal2453/luvit-meta', lazy = true },
+            { 'LelouchHe/xmake-luals-addon', lazy = true },
+        },
+        opts = {
+            library = {
+                { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+                { path = 'xmake-luals-addon/library', files = { 'xmake.lua' } },
+            },
+        },
+    },
+
+    {
+        'neovim/nvim-lspconfig',
+        cmd = 'LspInfo',
+        event = { 'BufReadPre', 'BufNewFile' },
+    },
+}
+
+require('lazy').setup(specs, opts)
--- vim/vim.vim
+++ vim/vim.vim
@@ -13,29 +13,6 @@ function! g:ConfigHome()
 endfunction
 command! CdConfig execute "cd " . ConfigHome()
 
-if has('gui_running') && !has('nvim')
-    set lines=45 columns=160
-    set guifont=SarasaMonoSC\ Nerd\ Font
-    set guioptions-=T
-endif
-
-if exists("g:neovide")
-    set guifont=SarasaMonoSC\ Nerd\ Font:h10
-    let g:neovide_opacity = 0.9
-    let g:neovide_normal_opacity = 0.9
-    let g:neovide_animation_length = 0
-    let g:neovide_cursor_trail_size = 0
-end
-
-" https://github.com/vim-airline/vim-airline/issues/2693
-function! s:AirlineAfterTheme()
-    hi StatusLine cterm=NONE gui=NONE
-    hi StatusLineNC cterm=NONE gui=NONE
-    hi StatusLineTerm cterm=NONE gui=NONE
-    hi StatusLineTermNC cterm=NONE gui=NONE
-endfunction
-autocmd User AirlineAfterTheme call s:AirlineAfterTheme()
-
 augroup vim_stuff
     autocmd!
     " Remember last edit position in files.
@@ -48,27 +25,13 @@ augroup vim_stuff
                 \ endif
 augroup END
 
-if !has('nvim')
-    let &t_SI = "\e[6 q"
-    let &t_SR = "\e[3 q"
-    let &t_EI = "\e[2 q"
-
-    set <M-H>=H
-    set <M-J>=J
-    set <M-K>=K
-    set <M-L>=L
-endif
-
 set belloff=all
 set nohidden
 set autoindent
-set nu
-set rnu
+set nu rnu
 if has('extra_search')
     set hls
 endif
-set list
-set isfname+=@-@
 set cursorline
 set ignorecase
 set smartcase
@@ -76,54 +39,96 @@ set shiftwidth=4
 set expandtab
 set cinoptions=l1N-sE-st0Lsg0
 set mouse=a
-set guicursor=n-v-c-sm:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkon0
-if has('nvim')
-    set guicursor+=t:block
-endif
+set guicursor=a:block-blinkon0
+set list
+set listchars=tab:>\ ,trail:-,extends:>,precedes:<,nbsp:+
 
-" vim-terminal-help
-let g:terminal_close = 1
-let g:terminal_list = 0
-let g:terminal_fixheight = 1
+set completeopt=menu,menuone,fuzzy,noselect
+set shortmess+=c
 
-if !exists("g:neovide")
-    " https://www.reddit.com/r/neovim/comments/zzs7eq/using_alacritty_but_no_transparency/
-    augroup theme_transparency_patch
-        autocmd!
-        autocmd ColorScheme * highlight Normal      ctermbg=NONE guibg=NONE
-        autocmd ColorScheme * highlight LineNr      ctermbg=NONE guibg=NONE
-        autocmd ColorScheme * highlight Folded      ctermbg=NONE guibg=NONE
-        autocmd ColorScheme * highlight NonText     ctermbg=NONE guibg=NONE
-        autocmd ColorScheme * highlight SpecialKey  ctermbg=NONE guibg=NONE
-        autocmd ColorScheme * highlight VertSplit   ctermbg=NONE guibg=NONE
-        autocmd ColorScheme * highlight SignColumn  ctermbg=NONE guibg=NONE
-        autocmd ColorScheme * highlight EndOfBuffer ctermbg=NONE guibg=NONE
-        " this is specific to how I like my tabline to look like
-        autocmd ColorScheme * highlight TablineFill ctermbg=NONE guibg=NONE
-    augroup END
-endif
+set <M-H>=H
+set <M-J>=J
+set <M-K>=K
+set <M-L>=L
 
-augroup desert_patch
+noremap  <M-H> <C-w>h
+noremap  <M-L> <C-w>l
+noremap  <M-J> <C-w>j
+noremap  <M-K> <C-w>k
+inoremap <M-H> <Esc><C-w>h
+inoremap <M-L> <Esc><C-w>l
+inoremap <M-J> <Esc><C-w>j
+inoremap <M-K> <Esc><C-w>k
+tnoremap <M-H> <C-\><C-n><C-w>h
+tnoremap <M-L> <C-\><C-n><C-w>l
+tnoremap <M-J> <C-\><C-n><C-w>j
+tnoremap <M-K> <C-\><C-n><C-w>k
+tnoremap <M-N> <C-\><C-n><C-w>p
+tnoremap <M-q> <C-\><C-n>
+
+function! s:OpenTerm(vertical, use_buffer_cwd)
+    let l:original_cwd = getcwd()
+    let l:buf_dir = expand('%:p:h')
+    if a:use_buffer_cwd
+        if isdirectory(l:buf_dir)
+            execute 'cd ' . fnameescape(l:buf_dir)
+        else
+            echoerr "Current buffer is not a file or directory does not exist."
+            return
+        endif
+    endif
+    if a:vertical
+        execute 'bot vert term'
+    else
+        execute 'bot term'
+    endif
+    if a:use_buffer_cwd
+        execute 'cd ' . fnameescape(l:original_cwd)
+    endif
+    startinsert
+endfunction
+
+command! Term   call s:OpenTerm(0, 0)
+command! Termv  call s:OpenTerm(1, 0)
+command! Termb  call s:OpenTerm(0, 1)
+command! Termbv call s:OpenTerm(1, 1)
+
+" https://www.reddit.com/r/neovim/comments/zzs7eq/using_alacritty_but_no_transparency/
+function! s:FixTransparency()
+    highlight Normal      ctermbg=NONE guibg=NONE
+    highlight LineNr      ctermbg=NONE guibg=NONE
+    highlight Folded      ctermbg=NONE guibg=NONE
+    highlight NonText     ctermbg=NONE guibg=NONE
+    highlight SpecialKey  ctermbg=NONE guibg=NONE
+    highlight VertSplit   ctermbg=NONE guibg=NONE
+    highlight SignColumn  ctermbg=NONE guibg=NONE
+    highlight EndOfBuffer ctermbg=NONE guibg=NONE
+    highlight TablineFill ctermbg=NONE guibg=NONE
+endfunction
+call s:FixTransparency()
+augroup theme_transparency_patch
     autocmd!
-    " only run these highlights when the colorscheme matches 'desert'
-    " a deep, matte green that blends with the warm tones of desert
-    autocmd ColorScheme desert highlight CursorLine cterm=NONE ctermbg=22 guibg=#2f4f2f
-    autocmd ColorScheme desert highlight SpecialKey cterm=NONE guibg=NONE
-    autocmd ColorScheme desert highlight NonText cterm=NONE guibg=NONE
-    autocmd ColorScheme desert highlight MatchParen ctermbg=236 guibg=#303030
+    autocmd ColorScheme * call s:FixTransparency()
 augroup END
-colorscheme desert
 
-IncScript keymaps.vim
+let g:mapleader = " "
+let g:maplocalleader = " "
+
+" window
+nnoremap <silent> <leader>= :resize +3<CR>
+nnoremap <silent> <leader>- :resize -3<CR>
+nnoremap <silent> <leader>, :vertical resize -3<CR>
+nnoremap <silent> <leader>. :vertical resize +3<CR>
+
+nnoremap <silent> <leader>u :UndotreeToggle<CR>
+
+nnoremap <silent> [q :cprevious<CR>
+nnoremap <silent> ]q :cnext<CR>
 
 function! g:RemoveTrailingWs()
     let saved = winsaveview()
     keeppatterns %s/\s\+$//e
     call winrestview(saved)
 endfunction
-command! RemoveTrailingWs call RemoveTrailingWs()
+command! RemoveTrailingWs call g:RemoveTrailingWs()
 command! Cclear cgetexpr [] | cclose
-
-if has('nvim')
-    packloadall
-endif
